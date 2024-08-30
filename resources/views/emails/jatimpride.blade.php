08483f0525835fba9901e30295950b537fa7b897
Kiki Rizka Abdullah <kikirabdullah@gmail.com>
2024-08-30 10:56:52 +0700

    update

--- resources/views/emails/jatimpride.blade.php
+++ resources/views/emails/jatimpride.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Transaksi Berhasil - {{ $item->no }}</title>
+    <title>Transaksi {{ $item->no }}</title>
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
 
