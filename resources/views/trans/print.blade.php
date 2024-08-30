9cdd803411e3a684c00be73db96144eb5c2b36bb
Kiki Rizka Abdullah <kikirabdullah@gmail.com>
2024-08-30 10:38:35 +0700

    update

--- resources/views/trans/print.blade.php
+++ resources/views/trans/print.blade.php
@@ -1,183 +1,251 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INVOICE - {{ $item->no }}</title>
-
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter:inter,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+        @page {
+            /* using padding , still same output */
+            margin: 0in;
+        }
 
         body {
-            background-color: #0000
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            font-size: 10pt;
+            background-color: black;
+            color: white;
         }
 
-        /** RTL **/
-        .invoice-box.rtl {
-            direction: rtl;
-            font-family: "Inter", sans-serif;
+        .invoice-container {
+            width: 95%;
+            padding: 20px;
+            position: relative;
+            overflow: hidden;
         }
 
-        .invoice-box.rtl table {
+        .watermark img {
+            position: absolute;
+            top: 35%;
+            left: 50%;
+            max-width: 80%;
+            opacity: 0.1;
+            transform: translate(-50%, -50%);
+            z-index: -1;
+        }
+
+        .invoice-header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 2px solid #ddd;
+            padding-bottom: 15px;
+        }
+
+        .invoice-header h3 {
+            font-size: 22px;
+            color: #ddd;
+        }
+
+        .company-info {
             text-align: right;
+            text-wrap: wrap;
         }
 
-        .invoice-box.rtl table tr td:nth-child(3) {
-            text-align: left;
+        .company-info h3 {
+            color: #fa0;
         }
 
-        .invoice-box {
-            max-width: 640px;
-            margin: auto;
-            padding: 50px;
-            border: 1px solid #cccccc;
-            box-shadow: 0 0 40px rgba(0, 0, 0, 0.15);
-            line-height: 24px;
-            font-family: "Inter", sans-serif;
-            color: #444;
-            font-size: 8pt !important;
+        .company-logo {
+            height: 50px;
+            margin-bottom: 10px;
         }
 
-        .invoice-box table {
+        .customer-info {
+            text-align: right;
+            padding: 15px;
+            border-radius: 5px;
+        }
+
+        .customer-info h2 {
+            color: #fa0;
+        }
+
+        .invoice-details h2 {
+            color: #fa0;
+        }
+
+        .invoice-details {
+            padding: 15px;
+            border-radius: 5px;
+        }
+
+        .transaction-details {
+            margin-top: 20px;
+        }
+
+        .transaction-details table {
             width: 100%;
-            line-height: inherit;
-            text-align: left;
+            border-collapse: collapse;
+            margin-top: 10px;
         }
 
-        .small-head {
-            text-transform: uppercase;
-            font-weight: 700;
-            line-height: 40px;
-            letter-spacing: 1px;
-        }
-
-        .invoice-box table td {
-            padding: 5px;
-            vertical-align: top;
-        }
-
-        .text-right {
-            text-align: right;
-        }
-
-        .invoice-box table tr.top table td {
-            padding-bottom: 10px;
-        }
-
-        .invoice-box table tr.top table td.title {
-            line-height: 45px;
-            color: #333;
-        }
-
-        .invoice-box table tr.information table td {
-            padding-bottom: 40px;
-        }
-
-        .invoice-box table tr.heading td {
-            background: #eee;
+        .transaction-details th,
+        .transaction-details td {
             border-bottom: 1px solid #ddd;
-            font-weight: bold;
-            padding: 5px 5px 8px 5px;
-
-        }
-
-        .invoice-box table tr.details td {
-            padding-bottom: 80px;
-        }
-
-        .invoice-box table tr.item td {
-            border-bottom: 1px solid #eee;
             padding: 10px;
         }
 
-        .invoice-box table tr.item.last td {
-            border-bottom: none;
-        }
-
-        .invoice-box table tr.total td {
-            border-top: 2px solid #eee;
+        .transaction-details th {
+            background-color: #f4f4f4;
             font-weight: bold;
-            padding-bottom: 40px;
         }
 
-        @media only screen and (max-width: 600px) {
-            .invoice-box table tr.top table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
+        .total {
+            color: #fa0;
+            text-align: right;
+            font-size: 10pt;
+            font-weight: bold;
+            margin-top: 10px;
+        }
 
-            .invoice-box table tr.information table td {
-                width: 100%;
-                display: block;
-                text-align: center;
-            }
+        .grandtotal {
+            color: #fa0;
+            text-align: right;
+            font-size: 12pt;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+
+        .thank-you {
+            text-align: center;
+            margin-top: 30px;
+            font-size: 12pt;
+            color: #ddd;
+            font-style: italic;
+        }
+
+        .paid-stamp {
+            position: absolute;
+            top: 250px;
+            left: 90px;
+            opacity: 0.8;
+        }
+
+        .paid-stamp img {
+            width: 200px;
         }
     </style>
 </head>
 
 <body>
-    <div class="invoice-box">
-        <table cellpadding="0" cellspacing="0">
-            <tr class="top">
-                <td colspan="2">
-                    <table>
-                        <tr>
-                            <td class="title">
-                                <img src="{{ public_path('app_local/img/logo.png') }}" style="max-height: 50px;" />
-                            </td>
+    <div class="invoice-container">
+        <div class="watermark">
+            <img src="{{ public_path('app_local/img/jp4.png') }}" alt="">
+        </div>
 
-                            <td class="text-right">
-                                <strong>{{ $item->no }}</strong><br />
-                                {{ $item->created_at->format('d/m/Y H:i:s') }}
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-
-            <tr class="information">
-                <td colspan="2">
-                    <table>
-
-                        <tr>
-                            <td>
-                                <span class="small-head">Customer:</span><br />
-                                {{ $item->customer->name ?? '' }}<br />
-                                {{ $item->customer->email ?? '' }}<br />
-                                {{ $item->customer->nowa ?? '' }}<br />
-                            </td>
-
-                            <td class="text-right">
-                                <span class="small-head">Detail:</span><br />
-                                {{ $item->jenisPengiriman->name ?? '' }}<br />
-                                {{ $item->text ?? '' }}<br />
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-
-
-
-            <tr class="heading">
-                <td>Item</td>
-                <td class="text-right">Harga</td>
-            </tr>
-            @foreach ($item->lines as $line)
-                <tr class="item">
-                    <td>
-                        <span style="font-weight: bold; padding-bottom:0;">{{ $line->merch->name_size }}</span><br>
-                        <small>{{ cleanNumber($line->merch->harga) }} x {{ cleanNumber($line->qty) }}</small>
+        <div class="invoice-header">
+            <table width="100%">
+                <tr>
+                    <td width="60%">
+                        <img src="{{ public_path('app_local/img/logo.png') }}" alt="Company Logo" class="company-logo">
                     </td>
-                    <td class="text-right">{{ cleanNumber($line->total) }}</td>
+                    <td width="40%">
+                        <div class="company-info">
+                            <h3><strong>{{ setting('contact_name') }}</strong></h3>
+                            <p>{{ setting('contact_email') }}</p>
+                            <p>{{ setting('contact_whatsapp') }}</p>
+                            <p>{{ setting('contact_alamat') }}</p>
+                        </div>
+                    </td>
                 </tr>
-            @endforeach
+            </table>
+        </div>
 
-            <tr class="total">
-                <td class="text-right">Total</td>
-                <td class="text-right">{{ cleanNumber($item->lines->sum('total')) }}</td>
-            </tr>
-        </table>
+        <div class="invoice-info">
+            <table width="100%">
+                <tr>
+                    <td style="vertical-align: top;">
+                        <div class="invoice-details">
+                            <h2>Invoice Details</h2>
+                            <p><strong>#{{ $item->no }}</strong></p>
+                            <p><strong>Status: </strong>{{ $item->status_str }}</p>
+                            <p><strong>Waktu Transaksi: </strong>{{ $item->created_at->format('d F Y H:i:s') }}</p>
+                            <p><strong>Jenis Pengiriman: </strong>{{ $item->jenisPengiriman->name ?? '' }} <br>
+                                <cite><small>{{ $item->jenisPengiriman->text ?? '' }}</small></cite>
+                            </p>
+
+                            @if ($item->jenis_pengiriman_id == 1)
+                                <p><strong>Alamat: </strong>{{ $item->alamat_prov ?? '' }}<br>
+                                    {{ $item->alamat ?? '' }}<br></p>
+                            @endif
+                            @if ($item->status == 'closed' && $item->jenis_pengiriman_id == 1)
+                                <p><strong>No Resi: </strong>{{ $item->noresi ?? '' }}<br></p>
+                            @endif
+                            <p><strong>Catatan: </strong><br>
+                                <cite><small>{{ $item->text ?? '' }}</small></cite>
+                            </p>
+
+                            @if ($item->status == 'rejected')
+                                <p><strong>Alasan Pembatalan: </strong><br>
+                                    <cite><small>{{ $item->text_reject ?? '' }}</small></cite>
+                                </p>
+                            @endif
+                        </div>
+                    </td>
+                    <td style="vertical-align: top;">
+                        <div class="customer-info">
+                            <h2>Customer Information</h2>
+                            <p><strong>Name: </strong>{{ $item->customer->name ?? '' }}</p>
+                            <p><strong>Email: </strong>{{ $item->customer->email ?? '' }}</p>
+                            <p><strong>Whatsapp: </strong>{{ $item->customer->nowa ?? '' }}</p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="transaction-details">
+            <table>
+                <tbody>
+                    @foreach ($item->lines as $line)
+                        <tr>
+                            <td width="70%">
+                                <span
+                                    style="font-weight: bold; padding-bottom:0;">{{ $line->merch->name_size }}</span><br>
+                                {{ cleanNumber($line->merch->harga) }} x {{ cleanNumber($line->qty) }} Pcs
+                            </td>
+                            <td style="text-align: right;" class="total">Rp {{ cleanNumber($line->total) }}</td>
+                        </tr>
+                    @endforeach
+                    <tr>
+                        <td width="70%" class="grandtotal">
+                            Total
+                        </td>
+                        <td style="text-align: right;" class="grandtotal">Rp
+                            {{ cleanNumber($item->lines->sum('total')) }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="thank-you">
+            <strong>Terima kasih atas pembelian merchandise Jatim Pride Vol. 4!</strong>
+            <p>Dukungan Anda sangat berarti bagi kami</p>
+            <br>
+            <p>Salam hangat,</p>
+            <strong class="grandtotal">{{ setting('contact_name') }}</strong>
+        </div>
+
+        @if ($item->status == 'closed')
+            <div class="paid-stamp">
+                <img src="{{ public_path('app_local/img/paid.png') }}" alt="PAID Stamp">
+            </div>
+        @endif
     </div>
 </body>
 
